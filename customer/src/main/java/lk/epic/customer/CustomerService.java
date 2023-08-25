962a6f2ea377316f827ae5b10ed4d421c37a3f43
Abhishek Ashinsa <abhishek_a@epic.lk>
2023-08-25 10:45:15 +0530

    initial commit for introduce config-server

--- customer/src/main/java/lk/epic/customer/CustomerService.java
+++ customer/src/main/java/lk/epic/customer/CustomerService.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Service
 @AllArgsConstructor
 public class CustomerService{
@@ -24,15 +22,17 @@ public class CustomerService{
 
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
 
+
+
+//        if (fraudCheckResponse.isFraudster()){
+//            throw new IllegalStateException("Fraudster");
+//        }
+
         );
-        
-        if (fraudCheckResponse.isFraudster()){
-            throw new IllegalStateException("Fraudster");
-        }
 
 
     }
